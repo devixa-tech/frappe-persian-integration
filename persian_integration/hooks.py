3e2825a06df10a143414568467576695bd79c35e
Mohammad Heydari <HMohammad2520@Gmail.com>
2026-02-13 13:28:58 +0330

    Added vazir fint

---
+++ persian_integration/hooks.py
@@ -0,0 +1,18 @@
+app_name = "persian_integration"
+app_title = "Persian Integration"
+app_publisher = "devixa-tech"
+app_description = "Adds better persian support for frappe framework"
+app_email = "hmohammad2520@gmail.com"
+app_license = "mit"
+
+
+fixtures = [
+    {"doctype": "Custom Field", "filters": {"module": "Persian Integration"}}
+]
+
+app_include_css = [
+    "/assets/jalali_shamsi_datepicker/css/vazirmatn-font.css",
+]
+app_include_js = [
+    # "/assets/jalali_shamsi_datepicker/js/persian-date-handler.js",
+]
